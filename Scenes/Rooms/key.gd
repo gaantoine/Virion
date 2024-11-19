bc74092ad55f83823d33d943af129197cb5fbc1d
JJesus1998 <163464834+JJesus1998@users.noreply.github.com>
2024-11-19 10:49:43 -0800

    Update key.gd

--- Scenes/Rooms/key.gd
+++ Scenes/Rooms/key.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 var player = null 
 
-func _ready():pass
+func _ready():
+    pass
 
 func follow_player():
     if player:
         global_position = player.global_position
+        global_position.x -= 5  

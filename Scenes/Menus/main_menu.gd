ec8c898e218a13d92d6401c9c976bcaa9f5f86e3
cyoce <masterryangp@gmail.com>
2024-12-17 12:18:01 -0800

    credits

--- Scenes/Menus/main_menu.gd
+++ Scenes/Menus/main_menu.gd
@@ -1,14 +1,13 @@
 extends Control
 
 func _ready() -> void:
-	print("READYYYYYYYYY")
+	pass
 
 func _exit_pressed() -> void:
-	print("aaaaaaaaaaaaa")
 	get_tree().quit()
 
 func _credits_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Menus/Credits.tscn")
 
 func _play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Rooms/MainLevel.tscn")
